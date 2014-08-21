30778fe3a549c20dffdc517c0087bff0e7960360
Jesse Sheehan <simplekiwi+git@gmail.com>
2014-08-21 18:05:18 +1200

    changed the library to a single function

--- index.js
+++ index.js
@@ -1,267 +1,180 @@
 /*
 * tokenize-anything - a module for tokenizing source code
 */
-module.exports = {
+
+/** 
+* Returns a token tree
+* 
+* @method _tokenize
+* @param {String} source
+* @param {Array} definitions
+* @param {Array} actions
+* @return {Array}
+*/
+"use_strict";
+
+var _tokenize = function(source, definitions, actions) {
   
-  /** 
-  * Returns an array of tokens with chainable properties
-  * 
-  * @method tokenize
-  * @param {Array} t_defs
-  * @param {String} source
-  * @return {Array}
-  */
-  tokenize: function(t_defs, source) {
-    var index = 0, t_list = [];
+    var source_index = 0, tokens = [];
     
-    // make each obj.patterns into a list and make every regex start with a caret
-    t_defs.map(function(c) {
-      c.patterns = [].concat(c.patterns)
+    // default definitions, actions  to []
+    if (_isnull(definitions)) definitions = [];
+    if (_isnull(actions)) actions = [];
+    
+    // properly format definitions[0..n].patterns
+    definitions.map(function(d) {
+      d.patterns = [].concat(d.patterns)
         .map(function(p,i,a) {
           if (p.source[0] != '^')
-            return a[i] = new RegExp('^' + p.source);
+            return /*a[i] = */new RegExp('^' + p.source);
           return p;
       });
     });
     
     // loop over each character of the source
-    while (index < source.length) {
+    while (source_index < source.length) {
       
       // get the remaining source code
-      var remaining_source = source.substr(index);
+      var remaining_source = source.substr(source_index);
       
       // iterate over each token definition until we find a match
-      for (var t_def_index = 0; t_def_index < t_defs.length; t_def_index++) {
-        
-        var t_name = t_defs[t_def_index].name,
-            t_patterns = t_defs[t_def_index].patterns;
-        
-        // if the token definition matches then push the new token and break
-        if (t_patterns.some(function(pattern) {
+      definitions.some(function(definition){
+        return definition.patterns.some(function(pattern) {
           
           var match = pattern.exec(remaining_source);
           
-          if (match !== null && typeof(match) != 'undefined') {
+          if (!_isnull(match)) {
             
-            // increment the index by the length of the matching text
-            index += match[0].length - 1;
+            // increment the source_index by the length of the matching text
+            source_index += match[0].length - 1;
             
             // push the new token onto the list
-            t_list.push({
-              'name': t_name,
-              'index': index,
+            tokens.push({
+              'name': definition.name,
+              'index': source_index,
               'value': match[0]
             });
             
-            return true;
+            return true; // match found, breaks out of the parent loop
           }
           
-          return false;
-        })) break;
-      }
+          return false; // no match found, continue loop
+        });
+      });
       
-      index++;
+      source_index++;
     }
     
-    return module.exports._make_chainable(t_list, t_defs);
-  },
-  
-  /**
-  *  Returns an array of tokens with chainable properties with the specified token(s) removed
-  * 
-  * @method remove
-  * @param {Array} t_list
-  * @param {Object} t_names
-  * @return {Array}
-  */
-  remove: function(t_list, t_names) {
-    t_names = [].concat(t_names); // in case t_names is a string
-    return module.exports._make_chainable(t_list.filter(function(x) {
-      return t_names.indexOf(x.name) == -1;
-    }), t_list.token_definitions);
-  },
-  
-  /**
-  * Returns true if the specified token is in the list
-  *
-  * @method contains
-  * @param {Array} t_list
-  * @param {String} t_name
-  * @return {Boolean}
-  */
-  contains: function(t_list, t_name) {
-    return t_list.some(function(x){return x.name == t_name;});
-  },
-  
-  /**
-  * Returns a chainable array of tokens with composite tokens defined
-  *
-  * @method compose
-  * @param {Array} t_list
-  * @param {String} t_name
-  * @param {String} open_edge
-  * @param {String} close_edge
-  * @param {Object} options
-  * @return {Array}
-  */
-  compose: function(t_list, t_name, open_edge, close_edge, options) {
-    var index = 0, new_t_list = [];
-    
-    var in_block = false, block_index, block_value;
-    
-    // indicates depth level for edge balancing, the exit depth is always 0
-    var depth = 0;
-    
-    // set the defaults for the options
-    if (options === null || typeof(options) == 'undefined') {
-      options = {
-        balance_edges: false,
-        include_edges: false,
-        preserve_edges: false,
-        recurse: false
-      };
-    } else {
-      if (options.preserve_edges === null || typeof(options.preserve_edges) == 'undefined')
-        options.preserve_edges = false;
-      if (options.balance_edges === null || typeof(options.balance_edges) == 'undefined')
-        options.balance_edges = false;
-      if (options.include_edges === null || typeof(options.include_edges) == 'undefined')
-        options.include_edges = false;
-      if (options.recurse === null || typeof(options.recurse) == 'undefined')
-        options.recurse = false;
-    }
-
-    while (index < t_list.length) {
-      
-      // If we are currently inside a block
-      if (in_block) {
-        
-        if (t_list[index].name == close_edge) {
-        
-          depth--;
-          
-          // If we are at the exit depth or aren't balancing edges...
-          if (options.balance_edges && depth === 0 || !options.balance_edges) {
+    // now apply actions (if any)
+    if (actions.length > 0) {
+      actions.map(function(action){
+        if (_isnull(action.action)) action.action = '';
+        switch (action.action) {
+          case 'remove': // do stuff
+            if (_isnull(action.names)) action.names = [];
+            action.names = [].concat(action.names); // in case names is a string
+            tokens = tokens.filter(function(token) { return action.names.indexOf(token.name) == -1; });
+            break;
+          case 'compose':
             
-            in_block = false;
+            var token_index = 0, new_tokens = [];
+    
+            var in_block = false, block_index, block_value, block_length;
+    
+            // indicates depth level for edge balancing, the exit depth is always 0
+            var depth = 0;
             
-            if (options.include_edges)
-              block_value += t_list[index].value;
+            if (_isnull(action.open) || _isnull(action.close) || _isnull(action.name)) break;
+             
+            // set the defaults for the options
+            if (_isnull(action.preserve)) action.preserve = false;
+            if (_isnull(action.balance)) action.balance = false;
+            if (_isnull(action.include)) action.include = false;
+            if (_isnull(action.recurse)) action.recurse = false;
+            if (_isnull(action.definitions)) action.definitions = definitions; // definitions for recursion
+            if (_isnull(action.actions)) action.actions = actions; // actions for recursion
             
-            var children = [];
-            	
-            if (typeof(options.recurse) == 'boolean') {
-              if (options.recurse === true) {
-                // tokenize the block using t_defs
-                children = module.exports.tokenize(t_list.token_definitions, block_value);
+            while (token_index < tokens.length) {
+              
+              // If we are currently inside a block
+              if (in_block) {
+                
+                if (tokens[token_index].name == action.close) {
+                
+                  depth--;
+                  
+                  // If we are at the exit depth or aren't balancing edges...
+                  if (action.balance && depth === 0 || !action.balance) {
+                    
+                    in_block = false;
+                      
+                    if (action.include)
+                      block_value += tokens[token_index].value;
+                    
+                    var children = [];
+                    	
+                    if (action.recurse === true) {
+                      // tokenize the composite block
+                      children = _tokenize(block_value, action.definitions, action.actions);
+                    }
+                    
+                    // Append the newly created block token to the new_t_list
+                    new_tokens.push({
+                      name: action.name,
+                      index: block_index,
+                      value: block_value,
+                      children: children
+                    });
+                    
+                    if (action.preserve)
+                      new_tokens.push(tokens[token_index]);
+                  } else {
+                    
+                    block_value += tokens[token_index].value;
+                  }
+                } else {
+                  
+                  if (tokens[token_index].name == action.open) {
+                    depth++;
+                  }
+                  
+                  // We are in the block but the current token is not an end token so we
+                  // add the value of the current token to the block_value
+                  block_value += tokens[token_index].value;
+                }
+              } else {
+                
+                // If we are in the block and the current block is a start token...
+                if (tokens[token_index].name == action.open) {
+                  
+                  depth++;
+                  
+                  if (action.preserve)
+                    new_tokens.push(tokens[token_index]);
+                    
+                  in_block = true;
+                  block_index = token_index + 1;
+                  block_value = action.include ? tokens[token_index].value : '';
+                } else {
+                  
+                  // If we aren't in the block and the current token isn't the starting token then
+                  // just push the current token onto the new_t_list
+                  new_tokens.push(tokens[token_index]);
+                }
               }
-            } else {
-              // tokenize the block using different token definitions
-              children = module.exports.tokenize(options.recurse, block_value); 
+              
+              token_index += 1;
             }
             
-            children = module.exports.compose(module.exports._make_chainable(children, t_list.token_definitions), t_name, open_edge, close_edge, options);
-            
-            // Append the newly created block token to the new_t_list
-            new_t_list.push({
-              name: t_name,
-              index: block_index,
-              value: block_value,
-              children: children
-            });
-            
-            if (options.preserve_edges)
-              new_t_list.push(t_list[index]);
-          } else {
-            
-            block_value += t_list[index].value;
-          }
-        } else {
-          
-          if (t_list[index].name == open_edge) {
-            depth++;
-          }
-          
-          // We are in the block but the current token is not an end token so we
-          // add the value of the current token to the block_value
-          block_value += t_list[index].value;
-        }
-      } else {
-        
-        // If we are in the block and the current block is a start token...
-        if (t_list[index].name == open_edge) {
-          
-          depth++;
-          
-          if (options.preserve_edges)
-            new_t_list.push(t_list[index]);
-          
-          in_block = true;
-          block_index = index + 1;
-          block_value = options.include_edges ? t_list[index].value : '';
-        } else {
-          
-          // If we aren't in the block and the current token isn't the starting token then
-          // just push the current token onto the new_t_list
-          new_t_list.push(t_list[index]);
-        }
-      }
-      
-      index += 1;
+            tokens = new_tokens;
+            break;
+        };
+      });
     }
     
-    return module.exports._make_chainable(new_t_list, t_list.token_definitions);
-  },
-  
- //  map: function(t_list, callback, options
-   
-  /**
-  * Returns a copy of the array with chainable properties
-  * 
-  * @method _make_chainable
-  * @param {Array} array
-  * @return {Array}
-  */
-  _make_chainable: function(array, t_defs) {
-    
-    // default t_defs to array.token_definitions or []
-    if (t_defs === null || typeof(t_defs) == 'undefined') {
-      if (array.token_definitions === null || typeof(array.token_definitions) == 'undefined') {
-        t_defs = [];
-      }  else {
-        t_defs = array.token_definitions;
-        // TODO: doesn't seem to work. never finds the parent definitions
-      }
-    }
-    
-    // Check whether this array has already been made chainable
-    if (array.is_chainable === null || typeof(array.is_chainable) == 'undefined' || array.is_chainable === false) {
-      
-      // Make a copy of the array, we do not perform operations on the argument
-      var array_copy = array.splice(0);
-      
-      array_copy.compose = function(t_name, open_edge, close_edge, options) {
-        return module.exports.compose(this, t_name, open_edge, close_edge, options);
-      };
-      
-      array_copy.remove = function(t_names) {
-        return module.exports.remove(this, t_names);
-      };
-      
-      array_copy.contains = function(t_name) {
-        return module.exports.contains(this, t_name);
-      };
-      
-      // Set the is_chainable variable so we don't make this array chainable again
-      array_copy.is_chainable = true;
-      
-      // Set the token_definitions
-      array_copy.token_definitions = t_defs;
-      
-      return array_copy;
-    }
-    
-    // Return the original array, it is already chainable, no need to add the methods to it
-    return array;
-  }
+    return tokens;
 };
+
+var _isnull=function(obj){return obj===null||typeof(obj)=='undefined';};
+
+module.exports = _tokenize;
